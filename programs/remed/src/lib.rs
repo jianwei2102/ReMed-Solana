fd389ff7ea1138c74d9e4f94072cbe06dc06d7d9
jianwei <jianwei2102@gmail.com>
2024-06-25 03:42:29 +0800

    build: solana program

--- programs/remed/src/lib.rs
+++ programs/remed/src/lib.rs
@@ -6,10 +6,73 @@ declare_id!("AaD2G2x6dkvNsUaHhKM26XBBVBfb76FsFtaZKtt4dnWj");
 pub mod remed {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn add_doc(ctx: Context<AddDoc>, doc: String) -> Result<()> {
+        ctx.accounts.permission_list.authorized.push(doc);
+
+        Ok(())
+    }
+
+    pub fn remove_doc(ctx: Context<RemoveDoc>, doc: String) -> Result<()> {
+        ctx.accounts
+            .permission_list
+            .authorized
+            .retain(|x| x != &doc);
+        Ok(())
+    }
+
+    pub fn append_record(ctx: Context<AppendRecord>, record: String) -> Result<()> {
+        if !ctx.accounts.permission_list.authorized.contains(&ctx.accounts.signer.key().to_string()) {
+            return Err(ErrorCode::Unauthorized.into());
+        }
+        ctx.accounts.medication_list.medication.push(record);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct AddDoc<'info> {
+    #[account(init_if_needed, payer = signer, space = 8 + 44*10, seeds = [b"permission_list", signer.key().as_ref()], bump)]
+    pub permission_list: Account<'info, PermissionList>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+
+#[derive(Accounts)]
+pub struct RemoveDoc<'info> {
+    #[account(mut, seeds = [b"permission_list", signer.key().as_ref()], bump)]
+    pub permission_list: Account<'info, PermissionList>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct AppendRecord<'info> {
+    #[account(seeds = [b"permission_list", patient.key().as_ref()], bump)]
+    pub permission_list: Account<'info, PermissionList>,
+    #[account(init_if_needed, payer = signer, space = 8 + 44*10, seeds = [b"medication_list1", patient.key().as_ref()], bump)]
+    pub medication_list: Account<'info, MedicationList>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    /// CHECK: This account is used to derive seeds for permission and medication lists.
+    pub patient: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct PermissionList {
+    authorized: Vec<String>,
+}
+
+#[account]
+pub struct MedicationList {
+    medication: Vec<String>,
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Unauthorized signer")]
+    Unauthorized,
+}
--- tests/remed.ts
+++ tests/remed.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Remed } from "../target/types/remed";
+import { SystemProgram, Keypair, PublicKey } from "@solana/web3.js";
 
 describe("remed", () => {
   // Configure the client to use the local cluster.
@@ -8,9 +9,136 @@ describe("remed", () => {
 
   const program = anchor.workspace.Remed as Program<Remed>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  // PLEASE DON'T HACK ME - 1
+  // user - 7JZKzBpbNAyGWrbpfNs61zZu9RWeUhhCC77UaLWcdocS 
+  const secretKeyBytes = [
+    69, 44, 51, 26, 12, 165, 235, 38, 77, 18, 86, 53, 244, 9, 197, 45, 191, 99,
+    238, 46, 27, 96, 115, 218, 254, 224, 244, 139, 194, 24, 93, 166, 93, 165,
+    140, 70, 236, 226, 163, 70, 11, 119, 129, 52, 157, 69, 40, 68, 8, 44, 177,
+    168, 121, 115, 102, 91, 63, 100, 188, 242, 32, 254, 182, 175,
+  ];
+
+  // PLEASE DON'T HACK ME - 2
+  // doc - 4GCuAtDNWAJn5LwLJirkfbrvmRZ7ruYdB4ZE5MyWCLG1
+  const secretKeyBytesDoc = [
+    196, 119, 8, 58, 119, 162, 112, 34, 211, 246, 240, 210, 91, 78, 183, 161,
+    202, 102, 64, 164, 80, 29, 99, 126, 101, 77, 38, 240, 140, 158, 65, 239, 48,
+    120, 58, 75, 67, 242, 59, 209, 116, 203, 40, 215, 125, 74, 95, 236, 45, 100,
+    58, 15, 85, 64, 255, 185, 23, 203, 194, 210, 15, 248, 194, 154,
+  ];
+  const user1 = Keypair.fromSecretKey(Uint8Array.from(secretKeyBytes));
+  const doc1 = Keypair.fromSecretKey(Uint8Array.from(secretKeyBytesDoc));
+
+  // it("Is initialized!", async () => {
+  //   // Add your test here.
+  //   // const tx = await program.methods.initialize().rpc();
+  //   console.log("Your transaction signature");
+  //   console.log(user1.publicKey.toString());
+  // });
+
+  // it("Add New Doc!", async () => {
+  //   // Generate keypair for the new account
+  //   const doc = new Keypair();
+
+  //   const seeds = [Buffer.from("permission_list"), user1.publicKey.toBuffer()];
+  //   const [permissionsAccount, nonce] =
+  //     await anchor.web3.PublicKey.findProgramAddress(seeds, program.programId);
+
+  //   await program.methods
+  //     .addDoc(doc1.publicKey.toString())
+  //     .accounts({
+  //       permissionList: permissionsAccount,
+  //       signer: user1.publicKey,
+  //       systemProgram: SystemProgram.programId,
+  //     })
+  //     .signers([user1])
+  //     .rpc();
+
+  //   console.log(
+  //     `Added doc (${doc.publicKey.toString()}) to the permission list.`
+  //   );
+  //   // // Fetch the created account
+  //   const permissionAccount = await program.account.permissionList.fetch(permissionsAccount);
+  //   console.log("On-chain data is:", permissionAccount.authorized.toString());
+  // });
+
+  // it("Remove Doc!", async () => {
+  //   const seeds = [Buffer.from("permission_list"), user1.publicKey.toBuffer()];
+  //   const [permissionsAccount, nonce] =
+  //     await anchor.web3.PublicKey.findProgramAddress(seeds, program.programId);
+
+  //   await program.methods
+  //     .removeDoc("93DqU4dtvCczEVzGqUKW8Uw6jxt2dQh9jkHzxho6xiru")
+  //     .accounts({
+  //       permissionList: permissionsAccount,
+  //       signer: user1.publicKey,
+  //       systemProgram: SystemProgram.programId,
+  //     })
+  //     .signers([user1])
+  //     .rpc();
+
+  //   console.log(`Removed doc to the permission list.`);
+  //   // // Fetch the created account
+  //   const permissionAccount = await program.account.permissionList.fetch(permissionsAccount);
+  //   console.log("On-chain data is:", permissionAccount.authorized.toString());
+  // });
+
+  // it("Display doc", async () => {
+  //   const seeds = [Buffer.from("permission_list"), user1.publicKey.toBuffer()];
+  //   const [permissionsAccount, nonce] = await anchor.web3.PublicKey.findProgramAddress(
+  //     seeds,
+  //     program.programId
+  //   );
+
+  //   // // Fetch the created account
+  //   const permissionAccount = await program.account.permissionList.fetch(permissionsAccount);
+  //   console.log("On-chain data is:", permissionAccount.authorized.toString());
+  // });
+
+  // it("Append record", async () => {
+  //   const seeds = [Buffer.from("permission_list"), user1.publicKey.toBuffer()];
+  //   const [permissionsAccount, nonce] =
+  //     await anchor.web3.PublicKey.findProgramAddress(seeds, program.programId);
+
+  //   const medicSeeds = [
+  //     Buffer.from("medication_list1"),
+  //     user1.publicKey.toBuffer(),
+  //   ];
+  //   const [medicationsAccount, mediNonce] =
+  //     await anchor.web3.PublicKey.findProgramAddress(
+  //       medicSeeds,
+  //       program.programId
+  //     );
+
+  //   try {
+  //     const txHash = await program.methods
+  //       .appendRecord("TEST2")
+  //       .accounts({
+  //         permissionList: permissionsAccount,
+  //         medicationList: medicationsAccount,
+  //         patient: user1.publicKey,
+  //         signer: doc1.publicKey,
+  //         systemProgram: SystemProgram.programId,
+  //       })
+  //       .signers([doc1])
+  //       .rpc();
+  // const medicalRecord = await program.account.medicationList.fetch(medicationsAccount);
+  // console.log("On-chain data is:", medicalRecord.medication.toString());
+  //     console.log(`Use 'solana confirm -v ${txHash}' to see the logs`);
+  //   } catch (error) {
+  //     console.error("Append failed:", error);
+  //   }
+  // });
+
+  it("Display records", async () => {
+    const medicSeeds = [Buffer.from("medication_list1"), user1.publicKey.toBuffer()];
+    const [medicationsAccount, medicNonce] = await anchor.web3.PublicKey.findProgramAddress(
+      medicSeeds,
+      program.programId
+    );
+
+    // // Fetch the created account
+    const medicalRecord = await program.account.medicationList.fetch(medicationsAccount);
+    console.log("On-chain data is:", medicalRecord.medication.toString());
   });
 });
